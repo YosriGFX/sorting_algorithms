336ec2229909b61d2fc0aa7f17c41cc27f7180d5
doubix <adib.grouz@gmail.com>
2020-02-04 15:14:19 +0100

    [update] condition

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -15,14 +15,17 @@ void insertion_sort_list(listint_t **list)
 	{
 		while (tmp)
 		{
-			if (tmp->n > tmp->next->n && tmp->next)
+			if (tmp->next)
 			{
-				n = tmp->n;
-				*(int *)&tmp->n = tmp->next->n;
-				*(int *)&tmp->next->n = n;
-				tmp = *list;
-				print_list(*list);
-				break;
+				if (tmp->n > tmp->next->n)
+				{
+					n = tmp->n;
+					*(int *)&tmp->n = tmp->next->n;
+					*(int *)&tmp->next->n = n;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
 			}
 			tmp = tmp->next;
 		}
