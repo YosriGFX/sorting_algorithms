ac9414966f45255fdb7fa6d71380844197b60460
YosriGFX <YosriG1997@gmail.com>
2020-02-04 22:44:14 +0100

    [init] Cocktail shaker sort

--- sort.h
+++ sort.h
@@ -25,6 +25,6 @@ void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void counting_sort(int *array, size_t size);
-
+void cocktail_sort_list(listint_t **list);
 
 #endif
